bed2d88eec4148990118f52632673582c471fa35
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-02-17 12:08:05 -0600

    test and build Player'shalth methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -16,6 +16,7 @@ test ('creates a player object', () => {
     );
 });
 
+
 test("gets player's stats as an object", () => {
     const player = new Player('Dave');
 
@@ -25,6 +26,7 @@ test("gets player's stats as an object", () => {
     expect(player.getStats()).toHaveProperty('agility');
 });
 
+
 test('gets inventory from player or returns false', () => {
     const player = new Player('Dave');
 
@@ -33,4 +35,36 @@ test('gets inventory from player or returns false', () => {
     player.inventory = [];
 
     expect(player.getInventory()).toEqual(false);
+});
+
+
+test("gets player's health value", () => {
+    const player = new Player('Dave');
+
+    expect(player.getHealth()).toEqual(expect.stringContaining(player.health.toString()));
+});
+
+
+test('checks is player is alive or not', () => {
+    const player = new Player('Dave');
+
+    expect(player.isAlive()).toBeTruthy();
+
+    player.health = 0;
+
+    expect(player.isAlive()).toBeFalsy();
+});
+
+
+test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+
+    player.reduceHealth(5);
+
+    expect(player.health).toBe(oldHealth - 5);
+
+    player.reduceHealth(99999);
+
+    expect(player.health).toBe(0);
 });
